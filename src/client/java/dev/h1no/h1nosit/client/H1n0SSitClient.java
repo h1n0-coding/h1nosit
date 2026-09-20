1378f97fc4422ab87354fafc4e5b70de9964f532
H1n0 <dimazhu2006@gmail.com>
2026-09-20 23:40:00 +0300

    Initial Fabric project for Minecraft 1.21.1

---
+++ src/client/java/dev/h1no/h1nosit/client/H1n0SSitClient.java
@@ -0,0 +1,10 @@
+package dev.h1no.h1nosit.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class H1n0SSitClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
